1974a2ad68c9cf2768a4e84465d5a62fcdba1ba1
ihbrazil <ihbrazil@users.noreply.github.com>
2024-11-17 17:01:25 -0300

    Atualização da 3ª Missão da Carreira Programador Mobile
    3ª Missão da Carreira Programador Mobile - Componentes - Criação do componente App

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Image, Text, View, StyleSheet } from 'react-native';
+
+import Logo from './assets/images/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={ Logo } style={styles.img} />
+      <Text style={styles.paragrafo}>
+       Windows 11 foi anunciado em 24 de junho de 2021, e desenvolvido pela Microsoft. É o sucessor do Windows 10, lançado seis anos antes.
+      </Text>
     </View>
   );
 }
@@ -13,8 +17,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#b6d4ed',
+    padding: 8,
+  },
+  img: {
+    width: 300,
+    height: 50,
+  },
+  paragrafo: {
+    marginTop: 24,
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
